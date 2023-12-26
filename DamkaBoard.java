1b35ff245294dc1313bfab1a09c1a7f7e948fc08
Idan Falach <idan.falach@post.runi.ac.il>
2023-12-26 21:14:04 +0200

    Damka board exercise

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,21 @@
 /**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
+ * Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
 public class DamkaBoard {
-	public static void main(String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        int sideLength = Integer.parseInt(args[0]);
+
+        for (int i = 1; i <= sideLength; i++) {
+            boolean isEvenLine = (i % 2 == 0);
+
+            for (int j = 0; j < sideLength; j++) {
+                if (isEvenLine) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
