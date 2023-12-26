09cc87c03255e78500e7c354cda5da470fbe5b64
Idan Falach <idan.falach@post.runi.ac.il>
2023-12-26 20:56:14 +0200

    Reverse exercise

--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,18 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
-	public static void main (String[] args){
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        String originalString = args[0];
+        String reversedString = "";
+        int stringLength = originalString.length();
+
+        for (int i = stringLength - 1; i >= 0; i--) {
+            reversedString += originalString.charAt(i);
+        }
+        System.out.println(reversedString);
+
+        int middleCharIndex = (stringLength - 1) / 2;
+        char middleChar = originalString.charAt(middleCharIndex);
+        System.out.println("The middle character is " + middleChar);
+    }
 }
