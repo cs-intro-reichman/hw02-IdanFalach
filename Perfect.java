70c2a151b82024f56e9b41b014fe35bea99f7523
Idan Falach <idan.falach@post.runi.ac.il>
2023-12-26 21:04:58 +0200

    Perfect numbers exercise

--- Perfect.java
+++ Perfect.java
@@ -1,8 +1,24 @@
 /**
- *  Gets a command-line argument (int), and chekcs if the given number is perfect.
+ * Gets a command-line argument (int), and chekcs if the given number is perfect.
  */
 public class Perfect {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+        int sumOfDivisors = 1; // 1 is divisor of every number
+
+        String perfectNumberMessage = number + " is a perfect number since " + number + " = 1";
+
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                sumOfDivisors += i;
+                perfectNumberMessage += " + " + i;
+            }
+        }
+
+        if (number == sumOfDivisors) {
+            System.out.println(perfectNumberMessage);
+        } else {
+            System.out.println(number + " is not a perfect number");
+        }
+    }
 }
