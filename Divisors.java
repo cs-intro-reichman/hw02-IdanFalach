19f780569477c86d1f2aa008a901e87d0bd55aba
Idan Falach <idan.falach@post.runi.ac.il>
2023-12-26 20:48:57 +0200

    fix indentation

--- Divisors.java
+++ Divisors.java
@@ -1,14 +1,14 @@
 /**
- *  Gets a command-line argument (int), and prints all the divisors of the given number.
+ * Gets a command-line argument (int), and prints all the divisors of the given number.
  */
 public class Divisors {
-	public static void main (String[] args) {
-		int number = Integer.parseInt(args[0]);
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
 
-		for (int i = 1; i <= number; i++) {
-			if ((number % i) == 0) {
-				System.out.println(i);
-			}
-		}
-	}
+        for (int i = 1; i <= number; i++) {
+            if ((number % i) == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
