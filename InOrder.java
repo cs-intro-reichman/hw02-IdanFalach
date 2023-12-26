ed3217c11250247c34abdf9caaca455b2a824929
Idan Falach <idan.falach@post.runi.ac.il>
2023-12-26 21:26:41 +0200

    Lucky streak exercise

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,16 @@
 /**
- *  Generates and prints random integers in the range [0,10),
- *  as long as they form a non-decreasing sequence.
+ * Generates and prints random integers in the range [0,10),
+ * as long as they form a non-decreasing sequence.
  */
 public class InOrder {
-	public static void main (String[] args) {
-		//// Write your code here
-	}
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() * 10);
+        int previousNumber = -1;
+
+        do {
+            System.out.print(randomNumber + " ");
+            previousNumber = randomNumber;
+            randomNumber = (int) (Math.random() * 10);
+        } while (randomNumber >= previousNumber);
+    }
 }
