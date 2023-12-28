043b04cc9dda3f9a3b2af2999522a2715ec4ee19
Idan Falach <idan.falach@post.runi.ac.il>
2023-12-28 22:07:30 +0200

    remove empty line

--- Perfect.java
+++ Perfect.java
@@ -5,7 +5,6 @@ public class Perfect {
     public static void main(String[] args) {
         int number = Integer.parseInt(args[0]);
         int sumOfDivisors = 1; // 1 is divisor of every number
-
         String perfectNumberMessage = number + " is a perfect number since " + number + " = 1";
 
         for (int i = 2; i < number; i++) {
